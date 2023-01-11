5b08c4c607b6bd7321096a5008a8722ee343ca38
ynxiyan <yn.xiyan@qq.com>
2023-01-11 17:11:17 +0800

    2023年01月11日提交

---
+++ Myinfo/src/com/qfedu/gather/collection/Student.java
@@ -0,0 +1,43 @@
+package com.qfedu.gather.collection;
+
+/**
+ * @Author: XIYAN
+ * @Date: 2023/1/10 17:59
+ * @注释:学生
+ */
+public class Student {
+    private String name;
+    private int age;
+
+    public Student() {
+    }
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
