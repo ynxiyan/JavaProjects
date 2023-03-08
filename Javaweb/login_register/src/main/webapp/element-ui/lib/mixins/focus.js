9f5ea671c8dff2d4bf970eabe20418e7166f0f2a
ynxiyan <yn.xiyan@qq.com>
2023-03-08 09:48:12 +0800

    由ynxiyan： 于2023年03月08日 提交...

---
+++ Javaweb/login_register/src/main/webapp/element-ui/lib/mixins/focus.js
@@ -0,0 +1,14 @@
+"use strict";
+
+exports.__esModule = true;
+
+exports.default = function (ref) {
+    return {
+        methods: {
+            focus: function focus() {
+                this.$refs[ref].focus();
+            }
+        }
+    };
+};
+
